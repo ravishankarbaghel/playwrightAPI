e600bb9ffdfb87d9fd2faa6b52121be2cd2d24b8
Ravishankar <ravishankarbaghel3008@gmail.com>
2024-06-11 18:25:08 +0530

    Playwright:adding Dispose Method

--- src/test/java/com/qa/api/tests/ValdateDispose.java
+++ src/test/java/com/qa/api/tests/ValdateDispose.java
@@ -1,10 +1,10 @@
 package com.qa.api.tests;
 
-import com.microsoft.playwright.APIRequest;
-import com.microsoft.playwright.APIRequestContext;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class ValdateDispose {
     Playwright playwright;
@@ -17,7 +17,51 @@ public class ValdateDispose {
         apiRequest=playwright.request();
         apiRequestContext=apiRequest.newContext();
     }
+@Test
+public void testDisposeMethod()
+{
+    //response 1
+    APIResponse response = apiRequestContext.get("https://gorest.co.in/public/v2/users");
+    Assert.assertEquals(response.statusText(),"OK");
+    System.out.println("----data before reponse dispose-------------");
+    System.out.println(response.text());
+     //now using dispose method at response level
 
+    response.dispose();
+    //trying to check if after using dispose method , do we get response body trying to use try catch block
+    System.out.println("----data after reponse dispose-------------");
+    try
+    {
+        System.out.println(response.text());
+    }catch (PlaywrightException e)
+    {
+        System.out.println("Playwright Exception as response has been dispose");
+    }
+    System.out.println( "though response body is disposed, but still we can get status code : "+ response.statusText());
+
+
+    ///response 2
+
+    APIResponse response2 = apiRequestContext.get("https://reqres.in/api/users/2");
+    System.out.println(response2.text());
+
+    System.out.println("----data after disponsing requestContext------------");
+    apiRequestContext.dispose();
+    try
+    {
+        System.out.println(response2.text());
+    }catch (PlaywrightException e)
+    {
+        System.out.println("Playwright Exception as requestContext has been disposed");
+    }
+    try
+    {
+        System.out.println(response.text());
+    }catch (PlaywrightException e)
+    {
+        System.out.println("Playwright Exception as Overall requestContext has been disposed");
+    }
+}
     @AfterTest
     public void tearDown()
     {
